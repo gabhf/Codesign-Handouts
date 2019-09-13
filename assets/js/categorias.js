e274dcd01bc478b5b9872e40ab61b6d494b0d7b9
Daniel Minson Pucciariello <daniel.minsonp@gmail.com>
2019-09-13 09:40:28 -0300

    lorem ipsun

--- assets/js/categorias.js
+++ assets/js/categorias.js
@@ -3,11 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     let params = coDesExtract()
 
     let value = params['key']
+    console.log(value)
+    var strLink = "projeto.html?pai=" + value + "&key={{@key}}"
+    let load = document.querySelector(".link1")
+    load.href=strLink
+
+    console.log(strLink)
 
     db.download('/', function(data) {
     context = data["portfolio"][value]
     console.log(context)
     coDesReplace('.lista', context)
-    coDesReplace('.nome_categ', context)    
+    coDesReplace('.nome_categ', context)
+     
     })
 })
